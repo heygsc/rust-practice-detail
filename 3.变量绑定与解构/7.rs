0e06b5fd6f3728489bc2c7047e66eaa23a0386b5
heygsc <103993866+heygsc@users.noreply.github.com>
2023-10-08 17:21:31 +0800

    Create 7.rs

---
+++ 3.变量绑定与解构/7.rs
@@ -0,0 +1,13 @@
+//修复编译器输出的 warning，不可以移除 let x = 1 所在的代码行
+fn main() {
+    let x = 1; 
+}
+// compiler warning: unused variable: `x`
+
+//答案
+fn main() {
+    let _x = 1; 
+}
+
+//讲解
+//下划线作为变量名的开头，就不会警告未使用的变量。
