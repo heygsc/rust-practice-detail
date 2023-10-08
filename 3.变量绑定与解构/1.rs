93721df600dcc128d71a021958cef7a5eca3ea42
heygsc <103993866+heygsc@users.noreply.github.com>
2023-10-08 11:22:40 +0800

    Update and rename index.md to 1.rs

---
+++ 3.变量绑定与解构/1.rs
@@ -0,0 +1,11 @@
+// 修复下面代码的错误并尽可能少的修改
+fn main() {
+    let x: i32 = 1; // 未初始化，但被使用
+    let y: i32; // 未初始化，也未被使用
+    println!("x is equal to {}", x); 
+}
+
+//x is equal to 1
+
+//println输出，用到了x。
+//x用let定义了，但未初始化，此处可以在x定义的同时初始化一个值。
