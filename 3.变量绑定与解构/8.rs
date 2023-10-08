b2e70096bd77b185fb62410e72cd72061c36be10
heygsc <103993866+heygsc@users.noreply.github.com>
2023-10-08 17:46:40 +0800

    Create 8.rs

---
+++ 3.变量绑定与解构/8.rs
@@ -0,0 +1,20 @@
+//题目
+// 修复下面代码的错误并尽可能少的修改
+fn main() {
+    let (x, y) = (1, 2);
+    x += 2;
+    assert_eq!(x, 3);
+    assert_eq!(y, 2);
+}
+
+//答案
+fn main() {
+    let (mut x, y) = (1, 2);
+    x += 2;
+
+    assert_eq!(x, 3);
+    assert_eq!(y, 2);
+}
+
+//讲解
+//加上mut使x可变，x+=2生效，从而x变为3
