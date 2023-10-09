b3ffac501db4ff2b182ee7bfceca603d2e78d9a6
heygsc <103993866+heygsc@users.noreply.github.com>
2023-10-09 14:30:32 +0800

    Create 3.rs

---
+++ 4.基本类型/4.1.数值类型/3.rs
@@ -0,0 +1,25 @@
+//题目
+// 修改 `assert_eq!` 让代码工作
+fn main() {
+    let x = 5;
+    assert_eq!("u32".to_string(), type_of(&x));
+}
+// 以下函数可以获取传入参数的类型，并返回类型的字符串形式，例如  "i8", "u8", "i32", "u32"
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
+
+//答案
+// 修改 `assert_eq!` 让代码工作
+fn main() {
+    let x = 5;
+    assert_eq!("i32".to_string(), type_of(&x));
+}
+// 以下函数可以获取传入参数的类型，并返回类型的字符串形式，例如  "i8", "u8", "i32", "u32"
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
+
+//讲解
+//rust整型默认使用i32，而不是u32。
+//所以在let x = 5之后，x是i32。
