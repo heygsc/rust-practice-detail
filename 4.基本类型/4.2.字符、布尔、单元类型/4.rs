53447b1795a181b8e3b94169d99f1868233d5439
heygsc <103993866+heygsc@users.noreply.github.com>
2023-10-11 15:44:39 +0800

    Update 4.rs

--- 4.基本类型/4.2.字符、布尔、单元类型/4.rs
+++ 4.基本类型/4.2.字符、布尔、单元类型/4.rs
@@ -1,1 +1,23 @@
+//题目
+fn main() {
+    let f = true;
+    let t = true && false;
+    assert_eq!(t, f);
 
+    println!("Success!")
+}
+
+//答案
+fn main() {
+    let f = true;
+    let t = true || false; // && 改为 ||
+    assert_eq!(t, f);
+
+    println!("Success!")
+}
+
+//讲解
+&&是同时满足，两个true才为true。
+||，两个里有一个true，即为true。
+f是true，为了让t和f相等，应该让t也是true。
+所以这里 && 改为 || 即可。
