e8b5e9fcfa46ff070b311bfd43a68a5e9d2e4038
heygsc <103993866+heygsc@users.noreply.github.com>
2023-10-11 14:21:34 +0800

    Create 9.rs

---
+++ 4.基本类型/4.1.数值类型/9.rs
@@ -0,0 +1,38 @@
+//题目
+//两个目标: 1. 修改 assert! 让它工作 2. 让 println! 输出: 97 - 122
+fn main() {
+    let mut sum = 0;
+    for i in -3..2 {
+        sum += i
+    }
+
+    assert!(sum == -3);
+
+    for c in 'a'..='z' {
+        println!("{}",c);
+    }
+}
+
+//答案
+fn main() {
+    let mut sum = 0;
+    //..2改成..=2
+    for i in -3..=2 {
+        sum += i
+    }
+
+    assert!(sum == -3);
+
+    for c in 'a'..='z' {
+        //加上as u8
+        println!("{}",c as u8);
+    }
+}
+
+//讲解
+// -3..2不包括2。
+// -3 + -2 + -1 + 0 + 1 = -5
+// 改成=2，把2加上，-5+2=-3，sum==-3等式成立。
+// 大写字母“A”到“Z”的ASCII码值为65到90。
+// 小写字母“a”到“z”的ASCII码值为97到122。
+// 加上as u8，把字母转成数字，即为ASCII码。a-z正好是97-122。
