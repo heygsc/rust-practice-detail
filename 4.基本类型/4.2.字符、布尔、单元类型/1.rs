3177ae5f38841d68a74338c64b6426591470d424
heygsc <103993866+heygsc@users.noreply.github.com>
2023-10-11 15:11:02 +0800

    Create 1.rs

---
+++ 4.基本类型/4.2.字符、布尔、单元类型/1.rs
@@ -0,0 +1,28 @@
+//题目
+// 修改2处 `assert_eq!` 让代码工作
+use std::mem::size_of_val;
+fn main() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),1); 
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),3); 
+
+    println!("Success!")
+} 
+
+//答案
+use std::mem::size_of_val;
+fn main() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),4); //1改为4
+
+    let c2 = '中';
+    assert_eq!(size_of_val(&c2),4); //3改为4
+
+    println!("Success!")
+} 
+
+//讲解
+// 字符类型占用 4 个字节。
+// 所以把1改为4，把3改为4。
