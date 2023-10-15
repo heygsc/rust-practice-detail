975f6f46a166b49755ed30fc23deb5d357829027
heygsc <103993866+heygsc@users.noreply.github.com>
2023-10-15 20:33:10 +0800

    Update 10.rs

--- 4.基本类型/4.1.数值类型/10.rs
+++ 4.基本类型/4.1.数值类型/10.rs
@@ -9,8 +9,8 @@ fn main() {
 //答案
 use std::ops::{Range, RangeInclusive};
 fn main() {
-    assert_eq!((1..5), Range{ start: 1, end: 5 });
-    assert_eq!((1..=5), RangeInclusive::new(1, 5));
+    assert_eq!((1..5), Range{ start: 1, end: 5 });  // 5
+    assert_eq!((1..=5), RangeInclusive::new(1, 5));  // =5
 }
 
 //讲解
