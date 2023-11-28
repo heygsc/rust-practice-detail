2ae67970d337b015f3711727b1732958f7100b96
heygsc <103993866+heygsc@users.noreply.github.com>
2023-11-28 14:49:46 +0800

    Update 2.rs

--- 3.变量绑定与解构/2.rs
+++ 3.变量绑定与解构/2.rs
@@ -9,13 +9,13 @@ fn main() {
 
 //答案
 fn main() {
-    let mut x = 1;//定义，初始化，mut可变
-    x += 2; //mut，可以变
+    let mut x = 1;//定义，初始化，mut 可变
+    x += 2; // mut，可以变
     
     println!("x = {}", x); 
 }
 //x = 3
 
 //讲解
-//println输出，用到了x，所以应该提前定义并初始化x的值。
-//rust中，默认一个值是不可变的。所以如果想要x+=2，需要加一个mut(mutable，本意为可变的)。
+// println 输出，用到了 x ，所以应该提前定义并初始化x的值。
+// rust 中，默认一个值是不可变的。所以如果想要 x+=2 ，需要加一个 mut ( mutable ，本意为可变的)。
