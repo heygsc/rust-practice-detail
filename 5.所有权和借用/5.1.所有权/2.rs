204bbdbcfb276ee972c33d7ac39f35c0a0600e9a
heygsc <103993866+heygsc@users.noreply.github.com>
2023-11-29 11:04:49 +0800

    Create 2.rs

---
+++ 5.所有权和借用/5.1.所有权/2.rs
@@ -0,0 +1,28 @@
+//题目
+// 不要修改 main 中的代码
+fn main() {
+    let s1 = String::from("hello, world");
+    let s2 = take_ownership(s1);
+
+    println!("{}", s2);
+}
+// 只能修改下面的代码!
+fn take_ownership(s: String) {
+    println!("{}", s);
+}
+
+//答案
+fn main() {
+    let s1 = String::from("hello, world");
+    let s2 = take_ownership(s1);
+
+    println!("{}", s2); // hello, world
+}
+fn take_ownership(s: String) -> String {
+    println!("{}", s); // hello, world
+    s
+}
+
+//讲解
+// println 后面语句，没有返回
+//这里用 s 作为返回，并加上 String 类型
